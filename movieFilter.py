e70126e7d65012f62794918866abe80e319a810d
hyh <141220041@smail.nju.edu.cn>
2017-01-25 19:17:28 +0800

    filter

--- movieFilter.py
+++ movieFilter.py
@@ -1,20 +1,24 @@
 #! /usr/bin/env python
 # coding=utf-8
-import os, json, platform
+import os, json, platform, sys
 from collections import OrderedDict
 
 filehome = os.getcwd()
-if (platform.system() == "Linux"):
+if platform.system() == "Linux":
     filehome = '/mnt/file/doubanMovie'
+
+reload(sys)
+sys.setdefaultencoding('utf-8')
 
 input = raw_input('Please input the NO or part of name: ')
 os.chdir(filehome)
 files = os.listdir(filehome)
-for file in files:
-    if (file.find('-') >= 0):
-        with open(filehome + '/' + file, 'r') as f:
+for f in files:
+    if f.find('-') >= 0:
+        with open(filehome + '/' + f, 'r') as f:
             for line in f:
                 if len(line.strip()) > 0:
                     item = json.loads(line, 'utf-8', object_pairs_hook=OrderedDict)
-                    if item['no'] == input or item['name'].find(input) >= 0:
+                    if (input.isdigit() and item['no'] == input) \
+                            or item['name'].find(input) >= 0:
                         print json.dumps(item, ensure_ascii=False)
